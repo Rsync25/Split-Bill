f6b3cd1c2d4f2817529ffae48ce054fb2c65f682
Rsync25 <rsync252@gmail.com>
2026-03-27 13:16:07 -0300

    fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,7 @@ const config = {
   solidity: "0.8.20",
   networks: {
     rskTestnet: {
+      type: "http",
       url: "https://public-node.testnet.rsk.co",
       chainId: 31,
       accounts: [process.env.PRIVATE_KEY]
